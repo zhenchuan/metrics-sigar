82a5fe748896667de4bf2fa2749d40924d540a7e
crnsnlzc <lzc6@vip.qq.com>
2014-06-27 01:32:35 +0800

    integrate into SigarMetrics and test

--- src/main/java/com/github/cb372/metrics/sigar/SigarMetrics.java
+++ src/main/java/com/github/cb372/metrics/sigar/SigarMetrics.java
@@ -3,7 +3,12 @@ package com.github.cb372.metrics.sigar;
 import com.codahale.metrics.Gauge;
 import com.codahale.metrics.MetricRegistry;
 
+import org.hyperic.sigar.ProcCpu;
 import org.hyperic.sigar.Sigar;
+import org.hyperic.sigar.SigarException;
+import org.hyperic.sigar.cmd.Ps;
+
+import java.util.List;
 
 public class SigarMetrics implements CanRegisterGauges {
     private static final SigarMetrics instance = new SigarMetrics();
@@ -16,7 +21,7 @@ public class SigarMetrics implements CanRegisterGauges {
     private final CpuMetrics cpu = new CpuMetrics(sigar); 
     private final MemoryMetrics memory = new MemoryMetrics(sigar); 
     private final FilesystemMetrics fs = new FilesystemMetrics(sigar); 
-    private final UlimitMetrics ulimit = new UlimitMetrics(sigar); 
+    private final UlimitMetrics ulimit = new UlimitMetrics(sigar);
 
     private SigarMetrics() {
         // singleton
@@ -54,4 +59,40 @@ public class SigarMetrics implements CanRegisterGauges {
     public UlimitMetrics ulimit() {
         return ulimit;
     }
+
+    public ProcessMetrics processMetrics(long pid){
+         return new ProcessMetrics(sigar,pid);
+    }
+
+    public ProcessMetrics processMetrics(String flag){
+        long pid = pid(flag);
+        if(pid == -1){
+            throw new IllegalArgumentException("can't find the process with your process flag : " + flag);
+        }
+        return new ProcessMetrics(sigar,pid);
+    }
+
+    public FileMetrics fileMetrics(String absoluteFilePath){
+        return new FileMetrics(sigar,absoluteFilePath);
+    }
+
+    public long pid(String flag){
+        try {
+            Ps ps = new Ps();
+            long[] pids = sigar.getProcList();
+            for (long pid : pids) {
+                List<String> info = ps.getInfo(sigar,pid);
+                if(info.size()>8){
+                    String name = info.get(8);
+                    if(name.contains(flag)){
+                        return pid;
+                    }
+                }
+            }
+        } catch (SigarException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
 }
--- src/test/java/com/github/cb372/metrics/sigar/SigarMetricsTest.java
+++ src/test/java/com/github/cb372/metrics/sigar/SigarMetricsTest.java
@@ -1,11 +1,17 @@
 package com.github.cb372.metrics.sigar;
 
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
+import org.hyperic.sigar.Sigar;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class SigarMetricsTest extends CheckSigarLoadsOk {
 
@@ -13,4 +19,29 @@ public class SigarMetricsTest extends CheckSigarLoadsOk {
     public void pidIsGreaterThanZero() throws Exception {
         assertThat(SigarMetrics.getInstance().pid(), is(greaterThan(0L)));
     }
+
+    @Test
+    public void testGetPID(){
+        assertTrue(SigarMetrics.getInstance().pid("java")>0);
+    }
+
+    @Test
+    public void testConsoleOut() throws InterruptedException {
+        final MetricRegistry registry = new MetricRegistry();
+        FileMetrics fileMetrics = SigarMetrics.getInstance().fileMetrics("/tmp") ;
+        ProcessMetrics metrics = SigarMetrics.getInstance().processMetrics("java");
+        metrics.registerGauges(registry);
+        fileMetrics.registerGauges(registry);
+        final ConsoleReporter reporter = ConsoleReporter.forRegistry(registry)
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build();
+
+        reporter.start(10,TimeUnit.SECONDS);
+
+        TimeUnit.SECONDS.sleep(60);
+
+    }
+
+
 }
